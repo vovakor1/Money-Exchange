832a0509c37e422d4970b918da8bc849fbce51ae
Vladimir <v.korotkin11@gmail.com>
2018-09-23 18:06:17 +0300

    money-exchange: solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-// PLEASE DON'T change function name
+const pars = {
+    H: 50,
+    Q: 25,
+    D: 10,
+    N: 5,
+    P: 1
+};
+const MAX_CURRENCY = 10000;
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency > MAX_CURRENCY) {
+        return {error: 'You are rich, my friend! We don\'t have so much coins for exchange'};
+    }
+
+    const coinsNumber = {};
+
+    if (currency >= pars.H) {
+        const rest = currency % pars.H;
+        coinsNumber.H = (currency - rest) / pars.H;
+        currency = rest;
+    }
+    if (currency >= pars.Q) {
+        const rest = currency % pars.Q;
+        coinsNumber.Q = (currency - rest) / pars.Q;
+        currency = rest;
+    }
+    if (currency >= pars.D) {
+        const rest = currency % pars.D;
+        coinsNumber.D = (currency - rest) / pars.D;
+        currency = rest;
+    }
+    if (currency >= pars.N) {
+        const rest = currency % pars.N;
+        coinsNumber.N = (currency - rest) / pars.N;
+        currency = rest;
+    }
+    if (currency >= pars.P) {
+        const rest = currency % pars.P;
+        coinsNumber.P = (currency - rest) / pars.P;
+    }
+
+    return coinsNumber;
+};
